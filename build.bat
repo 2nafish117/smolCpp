36318ee685c7cabf89d4319d343bebf8ce1022c4
shashank <shashank.shashank.c@gmail.com>
2019-12-20 13:04:37 +0530

    added build and clean batch files

---
+++ build.bat
@@ -0,0 +1,8 @@
+g++ .\Vignere\main.cpp -o .\Vignere\Vignere
+
+g++ .\Complex\Complex.cpp -c -o .\Complex\Complex.o
+g++ .\Complex\main.cpp .\Complex\Complex.o -o .\Complex\Complex
+
+g++ .\dynamic_array\dynamic_array.cpp -std=c++11 -o .\dynamic_array\dynamic_array
+
+g++ .\ParenBalance\main.cpp -std=c++11 -o .\ParenBalance\ParenBalance
